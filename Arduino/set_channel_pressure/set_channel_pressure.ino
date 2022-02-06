ab4a93dca612fb5f6d6d4ad4767d7c7b077b0c36
Brady King <brki7715@colorado.edu>
2022-02-05 21:58:17 -0700

    cleaning up cpp file

--- Arduino/set_channel_pressure/set_channel_pressure.ino
+++ Arduino/set_channel_pressure/set_channel_pressure.ino
@@ -6,24 +6,20 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <Wire.h>               // library for I2C connections
-#include "Adafruit_MPRLS.h"     // library for the pressure sensor
-
-// Pin definitions for the pressure sensor
-#define RESET_PIN  -1  // set to any GPIO pin # to hard-reset on begin()
-#define EOC_PIN    -1  // set to any GPIO pin to read end-of-conversion by pin
-
-#define TCAADDR 0x70   // address for the mux
-
-// Instantiate mpr class for pressure sensors
-Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
+#include <Wire.h>
+#include "Adafruit_MPRLS.h"
 
 // Channel/pressure related defines
-#define NUM_CHANNELS  1 // will be 3
+#define NUM_CHANNELS  3
+#define ON 1
+#define OFF 0
+// Turn channels on and off here!
+#define CHANNEL_1 OFF
+#define CHANNEL_2 ON
+#define CHANNEL_3 OFF
 #define DEFAULT_PRESSURE 12.0
 #define PRESSURE_HOLD_TOLERANCE 0.15
 #define PRESSURE_TOLERANCE 0.03
-#define HOLD_TIME_MS 500
 
 // I/O related defines
 #define SOLENOID_CLOSED LOW
@@ -33,8 +29,19 @@ Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
 #define PUMP_OFF 0
 
 // Serial related defines
-#define EXPECTED_MSG_LENGTH 4 // will be 15 bytes
-#define COMMAND_FREQUENCY_MS 1000 // milliseconds
+#define EXPECTED_MSG_LENGTH (4*(CHANNEL_1 + CHANNEL_2 + CHANNEL_3)) // 4 bytes per channel
+#define COMMAND_FREQUENCY_MS 100 // milliseconds
+
+// Enabled when pressure sensor functions are defined here in file
+#define LOCAL_PRESSURE_SENSOR_FUNCTIONS
+
+// Pin definitions for the pressure sensor
+#define RESET_PIN  -1  // set to any GPIO pin # to hard-reset on begin()
+#define EOC_PIN    -1  // set to any GPIO pin to read end-of-conversion by pin
+#define TCAADDR 0x70   // address for the mux
+
+// Instantiate mpr class for pressure sensors
+Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
  
 // Possible States
 typedef enum {
@@ -43,8 +50,10 @@ typedef enum {
     DEFLATE
 } state;
 
+// Data stored for each channel
 struct channelData 
 {
+    uint8_t active;
     state currentState;
     float currentPressure;
     float desiredPressure;
@@ -52,14 +61,13 @@ struct channelData
     int negativePump;
     int positiveSolenoid;
     int negativeSolenoid;
-    unsigned long startHoldTime;
 };
 
 channelData channels[NUM_CHANNELS] =
 {
-    // {HOLD, DEFAULT_PRESSURE, DEFAULT_PRESSURE, 3,  5,  2,  4},
-    {HOLD, DEFAULT_PRESSURE, DEFAULT_PRESSURE, 6,  9,  7,  8, 0},
-    // {HOLD, DEFAULT_PRESSURE, DEFAULT_PRESSURE, 10, 11, 12, 13},
+    {CHANNEL_1, HOLD, DEFAULT_PRESSURE, DEFAULT_PRESSURE, 3,  5,  2,  4},
+    {CHANNEL_2, HOLD, DEFAULT_PRESSURE, DEFAULT_PRESSURE, 6,  9,  7,  8},
+    {CHANNEL_3, HOLD, DEFAULT_PRESSURE, DEFAULT_PRESSURE, 10, 11, 12, 13},
 };
 
 unsigned long currentTime = 0;
@@ -70,7 +78,6 @@ unsigned long lastCommandTime = 0;
  * @desc  called once on startup
  */
 void setup() {
-  
     // scan for mux ports and begin serial communication
     scanner();
   
@@ -95,64 +102,19 @@ void loop() {
 
     // Read in serial command if needed...
     // Arduino serial buffer holds 64 bytes
-    // available() gets number of bytes available for reading in serial port
     if (((currentTime - lastCommandTime) >= COMMAND_FREQUENCY_MS) &&
         (Serial.available() >= EXPECTED_MSG_LENGTH))
     {
-        // Recieve command (format: "xx1000-2000-3000-")
-        // Contains desired PSI (implied decimal XX.XX) fr channels 0,1,2
-        
-        // Find start of command in serial buffer
-        char previous;
-        bool foundCommand = false;
-        while(Serial.available()) 
+        // Check for new command...
+        // Contains desired PSI (implied decimal XX.XX) for channels that are enabled
+        if (Serial.available() >= EXPECTED_MSG_LENGTH)
         {
-            if ((Serial.available() >= EXPECTED_MSG_LENGTH))
-            {
-                foundCommand = true;
-                break;
-            }
-            break;
-        }
+            handleCommand();
 
-        if (foundCommand == true)
-        {
-            char pressureVal[4];
-            // Read in pressure values for all channels
-            for (int8_t cNum = 0; cNum < NUM_CHANNELS; cNum++)
-            {
-                // Parse 4 characters for given channel
-                for (int8_t i = 0; i < 4; i++)
-                {
-                    char asc = Serial.read();
-                    pressureVal[i] = asc;
-                }
-                // Parse out delimeter'
-                // char delimeter = Serial.read();
-
-                char pressure[5];
-                sprintf(pressure, "%c%c.%c%c", pressureVal[0], pressureVal[1], pressureVal[2], pressureVal[3]);
-                
-                if (strncmp(pressure, "99.99", 5) == 0)
-                {
-                    // Read pressure command
-                    channels[cNum].currentPressure = get_pressure(mpr, 1);
-                    String pVal = String(channels[cNum].currentPressure, 2);
-                    Serial.println(pVal);
-                } 
-                else
-                {
-                    // Set pressure command
-                    channels[0].desiredPressure = ((String(pressure)).toFloat());
-                }
-                // Serial.println(channels[0].desiredPressure);
-            }
-            
             // Flush rest of input buffer
             while(Serial.available()) 
             {
                 char t = Serial.read();
-                // Serial.println(t);
             }
 
             // Update last command time
@@ -163,100 +125,144 @@ void loop() {
     // Update I/O if needed based on last command
     for (int8_t cNum = 0; cNum < NUM_CHANNELS; cNum++)
     {
-        channels[cNum].currentPressure = get_pressure(mpr, cNum);
-        // Serial.println(channels[cNum].currentPressure);
-        // Serial.println(channels[0].desiredPressure);
-
-        switch (channels[cNum].currentState)
+        if(channels[cNum].active)
         {
-            case INFLATE:
-                if ((channels[cNum].currentPressure >= (channels[cNum].desiredPressure - PRESSURE_TOLERANCE)) &&
-                    (channels[cNum].currentPressure <= (channels[cNum].desiredPressure + PRESSURE_TOLERANCE)) )
-                {
-                    channels[cNum].currentState = HOLD;
-                    channels[cNum].startHoldTime = currentTime;
-                }
-                else if (channels[cNum].currentPressure >= (channels[cNum].desiredPressure + PRESSURE_HOLD_TOLERANCE))
-                {
-                    channels[cNum].currentState = DEFLATE;
-                }
-                break; 
-                
-            case HOLD:
-                //if ((currentTime - channels[cNum].startHoldTime) >= HOLD_TIME_MS)
-                {
-                    if (channels[cNum].currentPressure <= (channels[cNum].desiredPressure - PRESSURE_HOLD_TOLERANCE))
+            channels[cNum].currentPressure = get_pressure(mpr, cNum);
+
+            switch (channels[cNum].currentState)
+            {
+                case INFLATE:
+                    if ((channels[cNum].currentPressure >= (channels[cNum].desiredPressure - PRESSURE_TOLERANCE)) &&
+                        (channels[cNum].currentPressure <= (channels[cNum].desiredPressure + PRESSURE_TOLERANCE)) )
                     {
-                        channels[cNum].currentState = INFLATE;
+                        channels[cNum].currentState = HOLD;
                     }
-                    else if (channels[cNum].currentPressure > (channels[cNum].desiredPressure + PRESSURE_HOLD_TOLERANCE))
+                    else if (channels[cNum].currentPressure >= (channels[cNum].desiredPressure + PRESSURE_HOLD_TOLERANCE))
                     {
                         channels[cNum].currentState = DEFLATE;
                     }
-                }
-                break;
-                
-            case DEFLATE:
-                if ((channels[cNum].currentPressure >= (channels[cNum].desiredPressure - PRESSURE_TOLERANCE)) &&
-                    (channels[cNum].currentPressure <= (channels[cNum].desiredPressure + PRESSURE_TOLERANCE)) )
-                {
-                    channels[cNum].currentState = HOLD;
-                    channels[cNum].startHoldTime = currentTime;
-                }
-                else if (channels[cNum].currentPressure <= (channels[cNum].desiredPressure - PRESSURE_HOLD_TOLERANCE))
-                {
-                    channels[cNum].currentState = INFLATE;
-                }
-                break;
-        }     
+                    break; 
+                    
+                case HOLD:
+                    {
+                        if (channels[cNum].currentPressure <= (channels[cNum].desiredPressure - PRESSURE_HOLD_TOLERANCE))
+                        {
+                            channels[cNum].currentState = INFLATE;
+                        }
+                        else if (channels[cNum].currentPressure > (channels[cNum].desiredPressure + PRESSURE_HOLD_TOLERANCE))
+                        {
+                            channels[cNum].currentState = DEFLATE;
+                        }
+                    }
+                    break;
+                    
+                case DEFLATE:
+                    if ((channels[cNum].currentPressure >= (channels[cNum].desiredPressure - PRESSURE_TOLERANCE)) &&
+                        (channels[cNum].currentPressure <= (channels[cNum].desiredPressure + PRESSURE_TOLERANCE)) )
+                    {
+                        channels[cNum].currentState = HOLD;
+                    }
+                    else if (channels[cNum].currentPressure <= (channels[cNum].desiredPressure - PRESSURE_HOLD_TOLERANCE))
+                    {
+                        channels[cNum].currentState = INFLATE;
+                    }
+                    break;
+            }     
 
-        switch (channels[cNum].currentState)
-        {
-            case INFLATE:
-                // Solenoids
-                digitalWrite(channels[cNum].positiveSolenoid, SOLENOID_OPEN);
-                digitalWrite(channels[cNum].negativeSolenoid, SOLENOID_CLOSED);
-                
-                // Pumps
-                analogWrite(channels[cNum].positivePump, PUMP_PWM_POS);
-                analogWrite(channels[cNum].negativePump, PUMP_OFF);
-                break; 
-                
-            case HOLD:
-                // Solenoids
-                digitalWrite(channels[cNum].positiveSolenoid, SOLENOID_CLOSED);
-                digitalWrite(channels[cNum].negativeSolenoid, SOLENOID_CLOSED);
-                
-                // Pumps
-                analogWrite(channels[cNum].positivePump, PUMP_OFF);
-                analogWrite(channels[cNum].negativePump, PUMP_OFF);
-                break;
-            
-            case DEFLATE:
-                // Solenoids
-                digitalWrite(channels[cNum].positiveSolenoid, SOLENOID_CLOSED);
-                if (channels[cNum].currentPressure >= 12.28)
-                {
-                    digitalWrite(channels[cNum].negativeSolenoid, SOLENOID_OPEN);
-                    delay(2);
+            switch (channels[cNum].currentState)
+            {
+                case INFLATE:
+                    // Solenoids
+                    digitalWrite(channels[cNum].positiveSolenoid, SOLENOID_OPEN);
                     digitalWrite(channels[cNum].negativeSolenoid, SOLENOID_CLOSED);
-                    delay(10);
-                }
-                else
-                {
-                    digitalWrite(channels[cNum].negativeSolenoid, SOLENOID_OPEN);
-                }
+                    
+                    // Pumps
+                    analogWrite(channels[cNum].positivePump, PUMP_PWM_POS);
+                    analogWrite(channels[cNum].negativePump, PUMP_OFF);
+                    break; 
+                    
+                case HOLD:
+                    // Solenoids
+                    digitalWrite(channels[cNum].positiveSolenoid, SOLENOID_CLOSED);
+                    digitalWrite(channels[cNum].negativeSolenoid, SOLENOID_CLOSED);
+                    
+                    // Pumps
+                    analogWrite(channels[cNum].positivePump, PUMP_OFF);
+                    analogWrite(channels[cNum].negativePump, PUMP_OFF);
+                    break;
+                
+                case DEFLATE:
+                    // Solenoids
+                    digitalWrite(channels[cNum].positiveSolenoid, SOLENOID_CLOSED);
+                    if (channels[cNum].currentPressure >= 12.28)
+                    {
+                        digitalWrite(channels[cNum].negativeSolenoid, SOLENOID_OPEN);
+                        delay(2);
+                        digitalWrite(channels[cNum].negativeSolenoid, SOLENOID_CLOSED);
+                        delay(6);
+                    }
+                    else
+                    {
+                        digitalWrite(channels[cNum].negativeSolenoid, SOLENOID_OPEN);
+                    }
 
-                // Pumps
-                analogWrite(channels[cNum].positivePump, PUMP_OFF);
-                analogWrite(channels[cNum].negativePump, PUMP_PWM_NEG); 
-                break;
-        }
-        
+                    // Pumps
+                    analogWrite(channels[cNum].positivePump, PUMP_OFF);
+                    analogWrite(channels[cNum].negativePump, PUMP_PWM_NEG); 
+                    break;
+            }
+        } 
     }
 
 }
 
+/*
+ * @name    commandHandler
+ * @desc    Handle command if found in serial buffer
+ * @param   None
+ * @return  None
+ */
+void handleCommand(void)
+{
+    char pressureVal[4];
+    // Read in pressure values for all channels
+    for (int8_t cNum = 0; cNum < NUM_CHANNELS; cNum++)
+    {
+        if(channels[cNum].active)
+        {
+            // Parse 4 characters for given channel
+            for (int8_t i = 0; i < 4; i++)
+            {
+                char asc = Serial.read();
+                pressureVal[i] = asc;
+            }
+
+            char pressure[5];
+            sprintf(pressure, "%c%c.%c%c", pressureVal[0], pressureVal[1], pressureVal[2], pressureVal[3]);
+            
+            if (strncmp(pressure, "99.99", 5) == 0)
+            {
+                // Read pressure command
+                channels[cNum].currentPressure = get_pressure(mpr, cNum);
+                String pVal = String(channels[cNum].currentPressure, 2);
+                Serial.println(pVal);
+            } 
+            else
+            {
+                // Set pressure command
+                channels[cNum].desiredPressure = ((String(pressure)).toFloat());
+            }
+        }
+    }
+}
+
+#if defined(LOCAL_PRESSURE_SENSOR_FUNCTIONS)
+/*
+ * @name    scanner
+ * @desc    Scan for connections to the multiplexer 
+ * @param   None
+ * @return  None
+ */
 void scanner()
 {
     while (!Serial);
@@ -264,70 +270,52 @@ void scanner()
     Wire.begin();
     
     Serial.begin(115200);
-    // Serial.println("\nTCAScanner ready!");
     
-    for (uint8_t t=0; t<8; t++) {
+    for (uint8_t t=0; t<8; t++) 
+    {
         tcaselect(t);
-        // Serial.print("TCA Port #"); 
-        // Serial.println(t);
 
         for (uint8_t addr = 0; addr<=127; addr++) 
         {
             if (addr == TCAADDR) 
             {
-              continue;
+                continue;
             }
             Wire.beginTransmission(addr);
-            if (!Wire.endTransmission()) 
-            {
-                //Serial.print("Found I2C 0x");  
-                // Serial.println(addr,HEX);
-            }
+            Wire.endTransmission();
         }
-    }   
-    //Serial.println("\nFinished scanning mux ports\n");
-    //Serial.println("------------------------------------------------------");
-    //delay(1000);
+    }
 }
 
+/*
+ * @name    sensor_initialization
+ * @desc    Used to initialize each of the pressure sensors
+ * @param   None
+ * @return  None
+ */
 void sensor_initialization()
 {
-    //Serial.println("Initializing pressure sensors:"); Serial.println("");
-    
-    // Init first sensor
-    tcaselect(0);
-    if (!mpr.begin())
+    for (uint8_t i = 0; i < 3; i++)
     {
-      //Serial.println("Error initializing Sensor 0");
-      while(1);
+        tcaselect(i);
+        if (!mpr.begin())
+        {
+            // Error Initializing sensor
+            while(1);
+        }
     }
-    //Serial.println("Initialized Sensor 0");
-
-    // Init second sensor
-    tcaselect(1);
-    if (!mpr.begin())
-    {
-      //Serial.println("Error initializing Sensor 1");
-      while(1);
-    }
-    //Serial.println("Initialized Sensor 1");
-    
-    // Init third sensor
-    tcaselect(2);
-    if (!mpr.begin())
-    {
-      //Serial.println("Error initializing Sensor 2");
-      while(1);
-    }
-    //Serial.println("Initialized Sensor 2\n");
-    //Serial.println("------------------------------------------------------");
-    //delay(1000);
 }
 
+/*
+ * @name    tcaselect
+ * @desc    Select device TCA is connected to
+ * @param   i - which port you want to connect to
+ * @return  None
+ */
 void tcaselect(uint8_t i) {
     if (i > 7)
     {
-      return;
+        return;
     }
    
     Wire.beginTransmission(TCAADDR);
@@ -335,12 +323,17 @@ void tcaselect(uint8_t i) {
     Wire.endTransmission();  
 }
 
+/*
+ * @name    get_pressure
+ * @desc    Function to return the pressure readings
+ * @param   readDelay - how frequent pressure will be logged
+ * @return  None
+ */ 
 float get_pressure(Adafruit_MPRLS mpr, uint8_t channelNum)
 {
-    tcaselect(1);
-    // tcaselect(channelNum);
+    tcaselect(channelNum);
     float pressure_hPa0 = mpr.readPressure();
     float presssure_PSI = (pressure_hPa0 / 68.947572932);
-    // Serial.println(presssure_PSI);
     return presssure_PSI;
 }
+#endif // LOCAL_PRESSURE_SENSOR_FUNCTIONS
