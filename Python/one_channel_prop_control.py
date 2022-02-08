eb03599e6950e20d87fa512517d1780d1019ac3c
Penumbrage <wwangVer2.0@gmail.com>
2022-02-08 10:10:51 -0700

    Added Shalom's version of del_P method

--- Python/one_channel_prop_control.py
+++ Python/one_channel_prop_control.py
@@ -84,17 +84,25 @@ class controllerThread(threading.Thread):
             print('Exception raise failure') 
         
 # < =============================== 1D Feedback Algorithm ===================================== >
-def one_D_feedback(z_des, z_act, P_act):
+def one_D_feedback(z_des, z_act, P_act, P_o):
     # define the proportional gain
     k_p = 1
 
     # Calculate the error between current and desired positions
-    eta_z = z_des - z_act
+    epsi_z = z_des - z_act
 
     # Multiply by the proportional gain k_p
-    P_des = k_p * eta_z
-    # del_P_des = k_p * eta_z
+    P_des = k_p * epsi_z
+
+    # < ------- Our feedback method --------- >
+    # del_P_des = k_p * epsi_z
     # P_des = P_act + del_P_des
+
+    # < -------- Shalom delta P method ------- >
+    # Figure out how to utilize del_P_act instead of P_des (on Arduino side?)
+    # del_P_des = k_p*epsi_z
+    # P_des = P_o + del_P_des
+    # del_P_act = P_des - P_act
 
     return P_des
 
