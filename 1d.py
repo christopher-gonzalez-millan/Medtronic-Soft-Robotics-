53a8324bf90af9c5dc01ec8ba4cd9090d6f76f47
Chritopher-Gonzalez <chgo7806@colorado.edu>
2022-02-15 11:07:32 -0700

    locking implemented for sharing variables

--- 1d.py
+++ 1d.py
@@ -3,17 +3,9 @@ import serial as pys
 import time
 import serial.tools.list_ports
 import threading
-
-stabilizationTime = 0.1
-class globalVars():
-    pass
-
-G = globalVars() #technically not a gloabl var
-G.lock = threading.Lock() #not really necessary in this case, but useful none the less
-G.value = 0
-G.kill = False
-G.zdes = None
-G.gainValue = 1
+c = threading.Condition() #pretty threading
+zDes = None
+gainValue = 1.0
 
 """Setup Variables
 
@@ -30,11 +22,20 @@ if (not ser.is_open):
     print("Serial Port Could not be Opened")
     quit()
 
+"""Helper Functions
+
+These varibale will hopefully never need to be changed
+"""
 def floatToCommand(float):
+    lessThanTen = False
+
+    if float < 10.0:
+        lessThanTen = True
+
     temp = str(float)
     temp = temp.replace('.', '')
 
-    if len(temp) == 2:
+    if lessThanTen:
         temp = '0' + temp
 
     if len(temp) == 3:
@@ -42,9 +43,9 @@ def floatToCommand(float):
 
     return temp
 
-def one_D_feedback(z_des, z_act):
+def one_D_feedback(z_des, z_act, gainValue):
     # define the proportional gain
-    k_p = G.gainValue
+    k_p = gainValue
 
     # Calculate the error between current and desired positions
     epsi_z = z_des - z_act
@@ -64,52 +65,86 @@ def one_D_feedback(z_des, z_act):
 
     return P_des
 
-def runFeedback(des):
-    while not G.kill:
-        if des != None:
+"""Feedback Thread
+
+These varibale will hopefully never need to be changed
+"""
+class Feedback(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name
+
+    def runFeedback():
+        #locking variables.
+        global zDes
+        global gainValue
+
+        while True:
+            c.acquire()
 
             try:
                 position = ndi.getPosition()
                 print("Current Position: {}".format(position.deltaX))
-                pressure =  one_D_feedback(des, position.deltaX)
+                pressure =  one_D_feedback(zDes, position.deltaX, gainValue)
                 pressure = round(pressure,2)
                 pressure = floatToCommand(pressure)
                 bytesSent = ser.write(pressure.encode('utf-8'))
-                time.sleep(stabilizationTime)
+                time.sleep(0.1)
 
             except:
                 print("There was an issue with NDISensor's parser")
-                #ndi.cleanup()
+                    #ndi.cleanup()
 
-        
+            c.release()
 
+"""Input Thread
 
-t1 = threading.Thread(target=runFeedback, args=(G.zdes,))
-t1.start()
+These varibale will hopefully never need to be changed
+"""
+class Input(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.name = name
 
-def askInput():
-    
-    userInput = input('Desire Position: ')
+    def runInput():
+        global zDes
+        global gainValue
 
-    if userInput =="quit" or userInput == "q":
-        print("Exiting")
-        ser.write(b'L')
-        ser.close()
-        G.kill = True
-        return False
-
-    if 'g' in userInput:
-        G.gainValue = float(userInput.replace('g', ''))
-        print("recivie gain of {}".format(G.gainValue))
+        while True:
+            c.acquire()
+            userInput = input('Desire Position: ')
 
 
-    elif userInput:
-        G.zdes = userInput
-        print("recivie positon of {}".format(userInput))
+            if userInput =="quit" or userInput == "q":
+                print("Exiting")
+                ser.close()
+                return False
 
-    return True
+            if 'g' in userInput:
+                gainValue =  float(userInput.replace('g', ''))
+                print("recivie gain of {}".format(gainValue))
 
-while askInput():
-    pass
 
-ser.close()
+            elif userInput:
+                zDes = userInput
+                print("recivie positon of {}".format(userInput))
+
+            c.release()
+
+def main():
+    '''Starting point for script
+    '''
+
+    a = Feedback("feedback")
+    b = Input("input")
+
+    b.start()
+    a.start()
+
+    a.join()
+    b.join()
+
+
+
+if __name__ == "__main__":
+    main()
