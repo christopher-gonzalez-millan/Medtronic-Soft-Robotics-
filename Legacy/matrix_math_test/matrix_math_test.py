96b61495004d68ed3d535f8e5d27896359ebdcb3
Penumbrage <wwangVer2.0@gmail.com>
2022-04-10 17:01:52 -0600

    created the files for developing the 3 channel algorithm

---
+++ Legacy/matrix_math_test/matrix_math_test.py
@@ -0,0 +1,6 @@
+#{
+# Filename: matrix_math_test.py
+# Author: William Wang (Team 7 Medtronic)
+# Description: This script tests the linear algebra capabilities for the 3 channel control algorithm
+# }
+
