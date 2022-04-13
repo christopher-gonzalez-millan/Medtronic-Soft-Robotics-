ba8c9481ec790475f1f1e8070cab37ae997d0a48
William Wang <wiwa3412@colorado.edu>
2022-04-13 16:33:02 -0600

    added safety checks on the pressure for the controller

--- Python/three_channel_open_control.py
+++ Python/three_channel_open_control.py
@@ -3,7 +3,7 @@
  * @author  CU Boulder Medtronic Team 7
  * @brief   Basic 1D proportional controller
 '''
-from NDI_Code.NDISensor import NDISensor
+# from NDI_Code.NDISensor import NDISensor
 from Py_Arduino_Communication.arduino_control import arduino_control
 import threading
 from queue import Queue
@@ -21,19 +21,30 @@ arduino.selectChannels(arduino.ON, arduino.ON, arduino.ON)
 
 while (True):
 
-    # get the actual pressure from the pressure sensor
-    print(arduino.getActualPressure(arduino.channel0))
-    print(arduino.getActualPressure(arduino.channel1))
-    print(arduino.getActualPressure(arduino.channel2))
+    if (input("Command: ") == "R"):
+        print(arduino.getActualPressure(arduino.channel0))
+        print(arduino.getActualPressure(arduino.channel1))
+        print(arduino.getActualPressure(arduino.channel2))
+    elif (input("Command: ") == "S"):
+        channel0 = input("Channel0: ")
+        if (channel0 != 0):
+            if (float(channel0) >= 16):
+                channel0 = 16
+            arduino.sendDesiredPressure(arduino.channel0, float(channel0))
 
-    channel0 = input("Channel0: ")
-    if (channel0 != 0):
-        arduino.sendDesiredPressure(arduino.channel0, float(channel0))
+        channel1 = input("Channel1: ")
+        if (channel1 != 0):
+            if (float(channel1) >= 16):
+                channel1 = 16
+            arduino.sendDesiredPressure(arduino.channel1, float(channel1))
 
-    channel1 = input("Channel2: ")
-    if (channel1 != 0):
-        arduino.sendDesiredPressure(arduino.channel1, float(channel1))
+        channel2 = input("Channel2: ")
+        if (channel2 != 0):
+            if (float(channel2) >= 16):
+                channel2 = 16
+            arduino.sendDesiredPressure(arduino.channel2, float(channel2))
 
-    channel2 = input("Channel2: ")
-    if (channel2 != 0):
-        arduino.sendDesiredPressure(arduino.channel2, float(channel2))
+        # get the actual pressure from the pressure sensor
+        print(arduino.getActualPressure(arduino.channel0))
+        print(arduino.getActualPressure(arduino.channel1))
+        print(arduino.getActualPressure(arduino.channel2))
