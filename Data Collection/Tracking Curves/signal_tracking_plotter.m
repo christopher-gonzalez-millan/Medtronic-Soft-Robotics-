e627dbcecbb6dbcbae40046643704835e5f33949
William Wang <wiwa3412@colorado.edu>
2022-04-16 00:33:21 -0600

    modified the plotter function for the 3 channel curves and plotted 3 channel curves for several tests

--- Data Collection/Tracking Curves/signal_tracking_plotter.m
+++ Data Collection/Tracking Curves/signal_tracking_plotter.m
@@ -11,43 +11,53 @@ close all
 
 %% Find all the csv files in the directory and obtain the data
 % specify directory of interest
-files = dir('3-30-22\*.xlsx');
+files = dir('4-16-22\*.xlsx');
 
 % get data from all the files in the directory
-[time_meas, time_plot, x_des, x_act] = get_data(files);
+[time_meas, time_plot, z_des, z_act, x_des, x_act] = get_data_three_channel(files);
 
 %% Plot the reference and tracked signals for the ramp input signal
-% specify lower and upper bounds of the ramp
-ramp_low_bd = [50, 50];
-ramp_up_bd = [80, 80];
+% % specify lower and upper bounds of the ramp
+% ramp_low_bd = [50, 50];
+% ramp_up_bd = [80, 80];
+% 
+% % specify the periods of the ramp
+% ramp_period = [20, 30];
+% 
+% % plot the ramp signals
+% ramp_time = [time_meas(1), time_meas(2)];         % reorganize ramp data
+% ramp_x_des = [x_des(1), x_des(2)];
+% ramp_x_act = [x_act(1), x_act(2)];
 
-% specify the periods of the ramp
-ramp_period = [20, 30];
-
-% plot the ramp signals
-ramp_time = [time_meas(1), time_meas(2)];         % reorganize ramp data
-ramp_x_des = [x_des(1), x_des(2)];
-ramp_x_act = [x_act(1), x_act(2)];
-
-ramp_plotter_func(ramp_time, ramp_x_des, ramp_x_act, ramp_period, ramp_low_bd, ramp_up_bd);
+% ramp_plotter_func(ramp_time, ramp_x_des, ramp_x_act, ramp_period, ramp_low_bd, ramp_up_bd);
 
 %% Plot the reference and tracked signals for the sinusoid input signal
-% specify upper and lower bounds of the sinusoid signal
-sin_low_bd = [70 55 70];
-sin_up_bd = [80 65 80];
+% % specify upper and lower bounds of the sinusoid signal
+% sin_low_bd = [70 55 70];
+% sin_up_bd = [80 65 80];
+% 
+% % specify the frequencies of the input signal
+% sin_freq = [0.05, 0.075, 0.1];
+% 
+% % plot the sinusoid signals for f = 0.05, 0.075, 0.1
+% % sinusoid_plotter_func(time_plot(4:6), x_des(4:6), x_act(4:6), sin_freq, sin_low_bd, sin_up_bd);
+% 
+% % plot the second set of sinusoid signals for f = 0.2, 0.5, 1
+% sin_low_bd = [70 70 70];
+% sin_freq = [0.2, 0.5, 1];
+% sin_up_bd = [80 80 80];
+% 
+% % sinusoid_plotter_func(time_plot(7:9), x_des(7:9), x_act(7:9), sin_freq, sin_low_bd, sin_up_bd);
 
-% specify the frequencies of the input signal
-sin_freq = [0.05, 0.075, 0.1];
+%% Plot reference and actual positions for the circle signal for the three channel prototype
+% radius of the circles in the plot
+radius = [15, 15, 15, 15, 15, 30];
 
-% plot the sinusoid signals for f = 0.05, 0.075, 0.1
-% sinusoid_plotter_func(time_plot(4:6), x_des(4:6), x_act(4:6), sin_freq, sin_low_bd, sin_up_bd);
+% specify the period of the circle signal
+circ_per = [60, 60, 60, 60, 60, 60];
 
-% plot the second set of sinusoid signals for f = 0.2, 0.5, 1
-sin_low_bd = [70 70 70];
-sin_freq = [0.2, 0.5, 1];
-sin_up_bd = [80 80 80];
-
-% sinusoid_plotter_func(time_plot(7:9), x_des(7:9), x_act(7:9), sin_freq, sin_low_bd, sin_up_bd);
+% plot the circle signals
+circle_plotter_func(time_plot, z_des, z_act, x_des, x_act, circ_per, radius);
 
 
 % < ===================================================================== >
@@ -114,6 +124,47 @@ for i=1:length(files)
     P_act(i) = {P_a};               % actual pressures
     k_prop(i) = {k_p};              % proportional gain
     k_int(i) = {k_i};               % integral gain
+    time_plot(i) = {time_devel_func(s, ms)};    % obtain the time differences between samples
+end
+
+end
+
+%% Function to iterate over files to read information in each file (3 Channel Case)
+function [time_meas, time_plot, z_des, z_act, x_des, x_act] = get_data_three_channel(files)
+% Description: This function reads all the data from a single directory and
+% stores the data into cells
+% Inputs: the files object that carries the information about the
+% different data files in a specific directory (files
+% Outputs: the time vector elapsed from the start of the experiment
+% (time_plot) or (time_meas) where time_plot is based off the logger time
+% and time_meas is based on the time difference from python, the actual and
+% desired positions in both x and z (x_act, x_des, z_act, z_des)
+
+% Initialize cells to store the values from the experiments
+date = {}; min = {}; sec = {}; millis = {}; time_meas = {}; samples = {}; z_des = {}; z_act = {};...
+    x_des = {}; x_act = {}; P_des = {}; P_act = {};
+
+% loop through the files and store the respective values
+for i=1:length(files)
+    filename = strcat(files(i).folder, '\', files(i).name);
+    [d, m, s, ms, samp, time_m, z_d, x_d, z_a, x_a, P_d_0, P_d_1, P_d_2, P_a_0, P_a_1, P_a_2]...
+        = readvars(filename); 
+    date(i) = {d};                  % date
+    min(i) = {m};                   % minutes
+    sec(i) = {s};                   % seconds
+    millis(i) = {ms};               % milliseconds
+    time_meas(i) = {time_m};        % time measured using the csv file
+    samples(i) = {samp};            % sample number
+    z_des(i) = {z_d};               % desired z positions
+    z_act(i) = {z_a};               % actual z position
+    x_des(i) = {x_d};               % desired positions
+    x_act(i) = {x_a};               % actual positions
+    P_des(1,i) = {P_d_0};           % desired pressures
+    P_des(2,i) = {P_d_1};
+    P_des(3,i) = {P_d_2};
+    P_act(1,i) = {P_a_0};           % actual pressures
+    P_act(2,i) = {P_a_1};
+    P_act(3,i) = {P_a_2};
     time_plot(i) = {time_devel_func(s, ms)};    % obtain the time differences between samples
 end
 
@@ -221,6 +272,37 @@ for i = 1:num_plots
     ylabel('x position [mm]');
     xlabel('time [sec]');
     title("Sinusoid Signal Response (" + low_bd(i) + " mm to " + up_bd(i) + " mm, f = " + freq(i) + " Hz)");
+end
+
+end
+
+%% Function to plot the circle responses
+function circle_plotter_func(time_plot, z_des, z_act, x_des, x_act, period, radius)
+% Description: this function is used to plot the circle signals
+% Inputs: the time vector (time_plot - cell), the desired and actual positions
+% (z_des, z_act, x_des, x_act - cell), the period of the signal (period - vector),
+% the radius of the circle (radius - vector)
+% Outputs: None
+
+% Determine the number of plots by counting the number of cells
+num_plots = length(time_plot);
+
+% loop to plot all the plots
+for i = 1:num_plots
+    % find the time where we have drawn one circle based on the period
+    for j = 1:length(time_plot{i})
+        if time_plot{i}(j) >= 60
+            break;
+        end
+    end
+    figure
+    plot(z_des{i}(1:j), x_des{i}(1:j),'LineWidth',1.5);        % reference circle
+    hold on 
+    plot(z_act{i}(1:j), x_act{i}(1:j),'LineWidth',1.5);        % actual circle
+    legend('Reference','Actual', 'Location', 'northeast');
+    xlabel('z position [mm]');
+    ylabel('x position [mm]');
+    title("Circle Signal Response (Radius = " + radius(i) + " mm, Period = " + period(i) + " sec)");
 end
 
 end
